b26fa88f5df893df78ddb4ab25db7310ac5ade6b
Gordan Zitkovic <gordanz@math.utexas.edu>
2020-07-21 13:01:49 -0500

    First commit.

---
+++ source/before-script.R
@@ -0,0 +1,14 @@
+## To be loaded before each chapter
+library(knitr)
+library(tidyverse)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  fig.align="center",
+  fig.pos="t"
+)
+set.seed(2016)
+options(digits = 4)
+options(dplyr.print_min = 4, dplyr.print_max = 4)
