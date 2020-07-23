0bbf82db7876bccc0e792af353ca8d13e6acdb3f
Gordan Zitkovic <gordanz@math.utexas.edu>
2020-07-23 13:32:32 -0500

    No_message

--- source/before-script.R
+++ source/before-script.R
@@ -3,12 +3,13 @@ library(knitr)
 library(tidyverse)
 
 knitr::opts_chunk$set(
-  comment = "#>",
+  comment = "#R>",
   collapse = TRUE,
   cache = TRUE,
   fig.align="center",
-  fig.pos="t"
+  fig.pos="t",
+  strip.white = TRUE
 )
 set.seed(2016)
-options(digits = 4)
+# options(digits = 4)
 options(dplyr.print_min = 4, dplyr.print_max = 4)
