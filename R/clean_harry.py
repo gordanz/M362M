3dc73c0ef1426a3777ae733063acd946056913d7
Gordan Zitkovic <gordanz@math.utexas.edu>
2020-08-27 18:55:22 -0500

    No_message

--- R/clean_harry.py
+++
@@ -1,9 +0,0 @@
-
-with open('h.txt', 'r') as file:
-    data = file.read()
-
-aux = data.split()
-aux2 = [ s.lower() for s in aux ]
-outdata = '\n'.join(aux2)
-with open('hw.txt',"w") as file:
-	file.write(outdata)
